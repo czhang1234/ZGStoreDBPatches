c098a3da0cc5d2f21f613246f41cafa0565980f8
czhang1234 <zhangxiao1234@hotmail.com>
2013-05-01 21:23:57 -0400

    Rename/drop columns from Users table

---
+++ Patches/Patch-01.07.sql
@@ -0,0 +1,54 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+DECLARE @Story varchar(max), @Sprint int, @Patch int, @Installed DateTime, @Prereqs int, @Desc varchar(max), @Ver varchar(200)
+
+Set @Story = 'DEV-1';
+Set @Sprint = 1;
+Set @Patch = 7;
+Set @Ver = CAST(@Sprint AS varchar) + '.' + CAST(@Patch AS varchar);
+Set @Desc = 'Drop and rename columns ';
+
+Print 'Executing... Patch v' + @Ver
+
+Select @Prereqs = isnull(Count(DeployedOn),0)  from DBVersion where Sprint=@Sprint and Patch<@Patch
+Select @Installed = DeployedOn  from DBVersion where Sprint=@Sprint and Patch=@Patch
+
+If(@Installed is null AND @Prereqs < @Patch)
+BEGIN
+BEGIN TRY
+BEGIN TRAN
+	
+exec sp_executesql N'
+alter table [dbo].[Users]
+drop column [EveningPhone]
+
+alter table [dbo].[Users]
+drop column [CellPhone]
+
+EXEC sp_rename ''Users.DayPhone'', ''Phone'', ''COLUMN''
+'
+
+
+
+Insert into DBVersion(Description, Sprint, Patch, Story, DeployedOn) values (@Desc, @Sprint, @Patch, @Story, GetDate())
+Print 'Patch v' + @Ver + ' was applied successfully '
+
+COMMIT
+END TRY
+BEGIN CATCH 
+Print 'An error occured and all transactions will be rolled-back'
+Print 'Error #' + CAST(ERROR_NUMBER() as varchar) + ': ' + ERROR_MESSAGE() + ' (Line: ' + CAST(ERROR_LINE() as varchar) + ')'
+ROLLBACK TRAN
+END CATCH
+END
+ELSE IF(@Installed is not null)
+BEGIN
+	Print 'Patch v' + @Ver + ' was already applied on ' + Convert(varchar(50), @Installed)  
+END 
+ELSE
+BEGIN
+	Print 'The patch could not be applied because your current schema is missing previous updates (Patch v' + @Ver + ')' 
+END
